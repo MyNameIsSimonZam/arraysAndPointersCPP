5c2b3e7fbc2e1499d40b0d4e9fc4c79485c66f19
Simon <semazamesin@gmail.com>
2025-05-03 21:37:44 +0500

    fourth task completed

--- Source.cpp
+++ Source.cpp
@@ -144,24 +144,40 @@ int numbers[15] = {114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 1
 Программа корректно использует обращения по индексам и выдаёт правильный ответ.
 Асимптотическая сложность алгоритма менее O(n^2).*/
 
+//void task4() {         // Insertion sort
+//  const int kArraySize = 15;
+//  int array[] = { 114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118 };
+//  int double_int;
+//
+//  for (int i = 1; i < kArraySize; ++i) {
+//    int temp = array[i];
+//    int j = i - 1;
+//    while (j >= 0 && array[j] < temp) {
+//      array[j + 1] = array[j];
+//      --j;
+//    }
+//    array[j + 1] = temp;
+//  }
+//
+//  for (int i = 0; i < kArraySize - 1; ++i) {
+//    if (array[i] == array[i + 1]) std::cout << array[i] << "\n";
+//  }
+//};
+
+
 void task4() {
   const int kArraySize = 15;
   int array[] = { 114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118 };
-  std::set<int> examin_array;
-  int double_int;
+  //int double_int;
 
+  int sum = 0;
   for (int i = 0; i < kArraySize; ++i) {
-    for (int j = 0; j < kArraySize; ++i) {
-
-    }
+    sum += array[i];
   }
-
-  for (int i = 0; i < kArraySize; ++i) {
-    std::cout << array[i] << " ";
-  }
+  std::cout << sum << "\n";
+  //std::cout << double_int << "\n";
+        // ИЩЕМ МИНИМАЛЬНОЕ ЗНАЧЕНИЕ И СЧИТАЕМ ПРОГРЕССИЮБ ПОТОМ ИЗ СУММЫ ВСЕХ ЧИСЕЛ ВЫЧИТАЕМ СУММЫ ПОСЛЕДОВАТЕЛЬНЫХ ЧИСЕЛ
 };
-
-
 
 
 
